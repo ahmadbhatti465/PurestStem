a6f72917635c621f351434ac28ee3fca225aceb1
ahmadbhatti.4 <ahmad765rajput@gmail.com>
2026-06-23 01:56:31 +0500

    correct

--- scripts/import-data.ts
+++ scripts/import-data.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import fs from "fs";
 
